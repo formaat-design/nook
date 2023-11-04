9c74db152599dff2f9c76e481d5211d61d736eb6
Dmitry Belyaev <blv.dmitry@gmail.com>
2023-11-04 13:37:22 +0100

    Initial widget setup and its toggle logic

--- packages/nook/src/components/Widget/Widget.tsx
+++ packages/nook/src/components/Widget/Widget.tsx
@@ -1,16 +1,56 @@
 "use client";
 
-import s from "./Widget.module.css"
-import { View, Text } from "reshaped"
+import s from "./Widget.module.css";
+import {
+  View,
+  Text,
+  Badge,
+  Actionable,
+  Icon,
+  useToggle,
+  useHotkeys,
+  classNames,
+} from "reshaped";
+import { useNook } from "../NookProvider";
+import IconCrosshair from "../../icons/Crosshair";
 
 const Widget = () => {
+  const nook = useNook();
+  const activeToggle = useToggle();
+  const rootClassNames = classNames(
+    s.root,
+    activeToggle.active && s["--active"],
+  );
+
+  useHotkeys({
+    "meta+i": activeToggle.toggle,
+  });
+
   return (
-    <div className={s.root}>
-      <View backgroundColor="elevation-overlay" borderRadius="medium" padding={2} paddingInline={3}>
-        <Text variant="caption-1" weight="medium">Nook</Text>
+    <View
+      backgroundColor="elevation-overlay"
+      borderColor="neutral-faded"
+      borderRadius="medium"
+      position="fixed"
+      className={rootClassNames}
+    >
+      <View direction="row" gap={3} padding={2} align="center">
+        <View.Item grow>
+          <Text variant="caption-1" weight="medium">
+            Nook
+          </Text>
+        </View.Item>
+        <Actionable onClick={activeToggle.toggle}>
+          <Icon
+            size={4}
+            svg={IconCrosshair}
+            color={activeToggle.active ? "primary" : "neutral"}
+          />
+        </Actionable>
+        <Badge>{Object.keys(nook.components).length} components</Badge>
       </View>
-    </div>
-  )
-}
+    </View>
+  );
+};
 
 export default Widget;

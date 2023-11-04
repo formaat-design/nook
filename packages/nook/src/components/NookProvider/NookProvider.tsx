36f25ba9514614a83c2d6aa24365d1e8229d9ff7
szympajka <szymon.pajka@gmail.com>
2023-11-04 21:41:49 +0000

    feat: use `nook-agent` websocket

--- packages/nook/src/components/NookProvider/NookProvider.tsx
+++ packages/nook/src/components/NookProvider/NookProvider.tsx
@@ -1,14 +1,38 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Widget from "../Widget";
 import type * as T from "./NookProvider.types";
+import mitt from "mitt";
+
+type Events = {
+  update: any;
+}
+
+export const eventBus = mitt<Events>();
 
 const Context = React.createContext({
   components: {},
 } as T.Context);
 
 export const useNook = () => React.useContext(Context);
+
+const useNookAgent = () => {
+  useEffect(() => {
+    // Create WebSocket connection.
+    const socket = new WebSocket("ws://localhost:51311");
+
+    // Connection opened
+    socket.addEventListener("open", (event) => {
+      socket.send("Hello Server!");
+    });
+
+    // Listen for messages
+    socket.addEventListener("message", (event) => {
+      eventBus.emit("update", JSON.parse(event.data));
+    });
+  }, []);
+}
 
 export const NookProvider = (props: T.Props) => {
   const { children } = props;
@@ -28,6 +52,8 @@ export const NookProvider = (props: T.Props) => {
       return next;
     });
   }, []);
+
+  useNookAgent();
 
   return (
     <Context.Provider value={{ components, register, unregister }}>
--- packages/nook/src/components/Widget/Widget.tsx
+++ packages/nook/src/components/Widget/Widget.tsx
@@ -1,13 +1,56 @@
 "use client";
 
+import React from "react";
 import s from "./Widget.module.css"
-import { View, Text } from "reshaped"
+import { View, Text, Loader } from "reshaped"
+import { eventBus } from "../NookProvider/NookProvider";
 
 const Widget = () => {
+  const [components, setComponents] = React.useState<any>({});
+
+  React.useEffect(() => {
+    const data = sessionStorage.getItem("nook");
+
+    if (data) {
+      try {
+        setComponents(JSON.parse(data));
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }, []);
+
+  React.useEffect(() => {
+    const update = (data: any) => {
+      setComponents(data.payload);
+      sessionStorage.setItem("nook", JSON.stringify(data.payload));
+    };
+
+    eventBus.on("update", update);
+
+    return () => {
+      eventBus.off("update", update);
+    };
+  }, []);
+
+  const componentsArray = Object.entries(components);
+
   return (
     <div className={s.root}>
-      <View backgroundColor="elevation-overlay" borderRadius="medium" padding={2} paddingInline={3}>
-        <Text variant="caption-1" weight="medium">Nook</Text>
+      <View backgroundColor="elevation-overlay" borderRadius="medium" padding={2} paddingInline={3} gap={3}>
+        {componentsArray.length === 0 && (
+          <Loader size='small' />
+        )}
+        {
+          componentsArray.map(([k, v]) => {
+            return (
+              <div key={k}>
+                <Text variant="body-3" weight="medium">{v as string}</Text>
+                <Text variant="caption-1" weight="regular">{k as string}</Text>
+              </div>
+            )
+          })
+        }
       </View>
     </div>
   )
